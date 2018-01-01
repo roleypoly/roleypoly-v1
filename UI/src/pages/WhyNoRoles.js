667666e53f664f2eba39a16b94f61a44a65d6b0c
Katalina / stardust <katie@player.me>
2017-12-31 23:23:40 -0600

    fix(UI/pages): utilize new rp-discord class

--- UI/src/pages/WhyNoRoles.js
+++ UI/src/pages/WhyNoRoles.js
@@ -8,10 +8,10 @@ const WhyNoRoles = (props) => {
     <h2>Why don't I see any roles in my editor?</h2>
     <p>Roleypoly needs to be a higher role position than other roles in order to assign them to anyone.</p>
     <h3 className="pages__bad">Bad <i uk-icon="icon: ban"></i></h3>
-    <img src={badImg} alt="Bad example"/>
+    <img src={badImg} className="rp-discord" alt="Bad example"/>
     <p>In this example, Roleypoly is at the bottom of the list. It can't assign anyone any roles above it.</p>
     <h3 className="pages__good">Good <i uk-icon="icon: check"></i></h3>
-    <img src={goodImg} alt="Good example"/>
+    <img src={goodImg} className="rp-discord" alt="Good example"/>
     <p>In this example, Roleypoly is above other roles, and will be able to assign them.</p>
   </Fragment>
 }
