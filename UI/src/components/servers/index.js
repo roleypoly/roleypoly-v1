7dd7c170b4a7bdaa2205df38dc7c0ba46ad53452
Katalina / stardust <katie@player.me>
2018-01-03 19:16:23 -0600

    feat(UI/servers): redirect to a usable page on /s/ path

--- UI/src/components/servers/index.js
+++ UI/src/components/servers/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Scrollbars } from 'react-custom-scrollbars'
 import { connect } from 'react-redux'
-import { withRouter } from 'react-router'
+import { withRouter, Redirect } from 'react-router'
 import './index.sass'
 
 import Navigation from './Navigation'
@@ -22,11 +22,21 @@ const mapState = ({ servers, user, appState }) => {
 
 @connect(mapState)
 class Servers extends Component {
+  get defaultPath () {
+    const first = this.props.servers.first()
+    if (first != null) {
+      return first.get('id')
+    }
+
+    return 'add'
+  }
+
   render () {
     return <div className="servers">
       <Navigation className="servers__nav" servers={this.props.servers} user={this.props.user} />
       <div className='servers__content'>
         <Switch>
+          <Route path='/s/' exact render={() => <Redirect to={`/s/${this.defaultPath}`} />} />
           <Route path='/s/:server/edit' component={RoleEditor} />
           <Route path='/s/:server' render={() =>
             <Scrollbars className={`fade-element ${(this.props.fade) ? 'fade' : ''}`} autoHeight autoHeightMax='calc(100vh - 80px)'>
