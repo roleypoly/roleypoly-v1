33dd3247083b1ffd05d00ada95c8f7c616949608
Kata <git@kat.cafe>
2019-05-20 06:27:45 -0400

    [design]: add color lib and helpers

---
+++ packages/roleypoly-design/src/helpers/colors.ts
@@ -0,0 +1,16 @@
+import * as Color from 'color'
+
+const color = (i: Color | string) => {
+  return new Color(i)
+}
+
+export const stepUp = (input: Color | string): string => {
+  return color(input).lighten(0.1).string()
+}
+export const stepDown = (input: Color | string): string => {
+  return color(input).darken(0.1).string()
+}
+
+export const textMode = (bg: Color | string, light: string = '#efefef', dark: string = '#1c1111'): string => {
+  return color(bg).isDark() ? light : dark
+}
