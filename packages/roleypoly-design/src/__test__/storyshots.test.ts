5b466670f30171c3c3ee1883f383aa6763aba720
Kata <git@kat.cafe>
2019-05-20 04:40:44 -0400

    [design]: fix storyshots, update snapshots

--- packages/roleypoly-design/src/__test__/storyshots.test.ts
+++ packages/roleypoly-design/src/__test__/storyshots.test.ts
@@ -1,9 +1,12 @@
+/* jest-env jsdom */
 import * as path from 'path'
 import initStoryshots from '@storybook/addon-storyshots'
+import { shallow } from 'enzyme'
 // import Adapter from 'enzyme-adapter-react-16'
 
-// enzyme.configure({ adapter: new Adapter() })
+// configure({ adapter: new Adapter() })
 
 initStoryshots({
+  renderer: shallow,
   configPath: path.resolve(__dirname, '../../.storybook')
 })
