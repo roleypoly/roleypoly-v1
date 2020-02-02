dc4400aa343ea7a98368da0e828fd9052f1f3c4c
Kata <git@kat.cafe>
2020-02-02 14:27:33 -0500

    fix(ServerCard): black names should be white

--- UI/src/utils.js
+++ UI/src/utils.js
@@ -2,4 +2,10 @@ import color from 'color'
 
 export const msgToReal = msg => msg.replace(/</g, '&lt;').replace(/\n/g, '<br />')
 
-export const colorToCSS = colorInt => color(colorInt).hsl().string()
+export const colorToCSS = colorInt => {
+    if (colorInt === 0) {
+        return '#fff'
+    }
+    
+    return color(colorInt).hsl().string()
+}
