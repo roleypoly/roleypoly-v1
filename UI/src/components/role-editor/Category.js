1ad043aff9f6bb8ab328b4e1e129e99e627b0731
Katalina <git@kat.cafe>
2020-04-26 18:37:28 -0400

    fix(Editor): add sortBy on role position to fix non-deterministic random placements

--- UI/src/components/role-editor/Category.js
+++ UI/src/components/role-editor/Category.js
@@ -38,6 +38,7 @@ class Category extends Component {
         </div>
         {category
           .get('roles_map')
+          .sortBy(r => r.get('position'))
           .reverse()
           .map((r, k) => <Role key={k} role={r} categoryId={name} />)
           .toArray()}
